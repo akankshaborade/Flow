94ec0a4101dbc99171bd573a452997af9026b021
gharatgargi-7 <gharatgargi@gmail.com>
2026-03-21 16:36:44 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ app.use('/api/auth', require('./routes/auth'));
 app.use("/api/tasks", require("./routes/tasks"));
 app.use("/api/clients", require("./routes/clients"));
 app.use("/api/projects", require("./routes/projects"));
+app.use("/api/dashboard", require("./routes/dashboard"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
