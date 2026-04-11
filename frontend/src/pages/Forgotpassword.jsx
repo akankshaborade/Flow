c05ef40b6b826793fed1fb04981225ba0525acab
gharatgargi-7 <gharatgargi@gmail.com>
2026-04-11 13:42:29 +0530

    Update Forgotpassword.jsx

--- frontend/src/pages/Forgotpassword.jsx
+++ frontend/src/pages/Forgotpassword.jsx
@@ -1,1 +1,20 @@
+import { Link } from "react-router-dom";
+import AuthLayout from "../components/AuthLayout";
 
+const ForgotPassword = () => {
+  return (
+    <AuthLayout>
+      <h2>Reset Password</h2>
+
+      <input placeholder="Enter your email" />
+
+      <button>Send Reset Link →</button>
+
+      <p>
+        <Link to="/login" className="link">← Back to Login</Link>
+      </p>
+    </AuthLayout>
+  );
+};
+
+export default ForgotPassword;
