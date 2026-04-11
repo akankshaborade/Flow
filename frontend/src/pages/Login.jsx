210466e1ee07eb60abbb576c3b1e471b7eaa0a9d
gharatgargi-7 <gharatgargi@gmail.com>
2026-04-11 13:40:35 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,1 +1,26 @@
+import { Link } from "react-router-dom";
+import AuthLayout from "../components/AuthLayout";
 
+const Login = () => {
+  return (
+    <AuthLayout>
+      <h2>Welcome back</h2>
+
+      <input placeholder="Email" />
+      <input type="password" placeholder="Password" />
+
+      <button>Login →</button>
+
+      <p>
+        <Link to="/forgot" className="link">Forgot Password?</Link>
+      </p>
+
+      <p>
+        Don’t have an account?{" "}
+        <Link to="/signup" className="link">Sign up</Link>
+      </p>
+    </AuthLayout>
+  );
+};
+
+export default Login;
