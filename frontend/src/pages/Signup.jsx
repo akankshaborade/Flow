1a43089a8c7a467ecb834669b34b155be26ebdd3
gharatgargi-7 <gharatgargi@gmail.com>
2026-04-11 13:41:32 +0530

    Update Signup.jsx

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,1 +1,24 @@
+import { Link } from "react-router-dom";
+import AuthLayout from "../components/AuthLayout";
 
+const Signup = () => {
+  return (
+    <AuthLayout>
+      <h2>Create an account</h2>
+
+      <input placeholder="First Name" />
+      <input placeholder="Last Name" />
+      <input placeholder="Email" />
+      <input type="password" placeholder="Password" />
+
+      <button>Create Account →</button>
+
+      <p>
+        Already have an account?{" "}
+        <Link to="/login" className="link">Login</Link>
+      </p>
+    </AuthLayout>
+  );
+};
+
+export default Signup;
