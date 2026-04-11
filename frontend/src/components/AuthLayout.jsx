357c4bf4264e5f59d73d6f209b062b79b0551adb
gharatgargi-7 <gharatgargi@gmail.com>
2026-04-11 13:37:31 +0530

    Update AuthLayout.jsx

--- frontend/src/components/AuthLayout.jsx
+++ frontend/src/components/AuthLayout.jsx
@@ -1,1 +1,25 @@
+import { Link } from "react-router-dom";
 
+const AuthLayout = ({ children }) => {
+  return (
+    <div className="container">
+      {/* LEFT */}
+      <div className="left-panel">
+        <h2>Flow</h2>
+
+        <div>
+          <h1>Capturing Moments,<br />Creating Memories</h1>
+        </div>
+
+        <Link to="/" className="link">← Back to website</Link>
+      </div>
+
+      {/* RIGHT */}
+      <div className="right-panel">
+        <div className="card">{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default AuthLayout;
